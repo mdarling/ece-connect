bfe8824d03582100e8a79990e7981fd1184921fd
mdarling <michaelcdarling@gmail.com>
2014-08-10 11:17:40 +0530

    Created application using Refinery CMS 2.1.2

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EceConnect::Application.config.secret_token = '99c6a047c7edfc76caecaf1984a9e5e22dc13d6c8aca392d1d9ea847f52ba835c95db5492d42b180a0876e83e72e6cd6ddb9db5724e9cc462933bbe1f8c0b06a'
